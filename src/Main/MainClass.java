3dcae28b1b6f96104b11d48f3b14ca6c640851db
VincenzoLaSpesa <vincenzolaspesa@gmail.com>
2016-03-26 11:10:22 +0100

    boh

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -16,6 +16,9 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import joptsimple.OptionParser;
@@ -36,16 +39,16 @@ public class MainClass {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws InterruptedException {
 
         //Loads an image.
         FastBitmap fb = new FastBitmap("pp.png");
 
         List<FastBitmap> livelli = ATrousWavelet.applyTransform(fb, 8);
-
-        HistogramStretchLinear hs = new HistogramStretchLinear();
-
+        final HistogramStretchLinear hs = new HistogramStretchLinear();
+             
         int n = 0;
         StringBuilder sb = new StringBuilder("[");
         for (FastBitmap l : livelli) {
@@ -57,20 +60,23 @@ public class MainClass {
         }
         char[] json = sb.toString().toCharArray();
         json[json.length - 1] = ']';
+
         
+        /*ExecutorService executor= Executors.newWorkStealingPool();
+        executor.awaitTermination(1, TimeUnit.DAYS);*/
         tryWriteFile("levels.json", json.toString());
 
         //Show the result.
         MultipleImageViewer.show(livelli, Arrays.asList("Livello 1", "Livello 2", "Livello 3", "Livello 4", "Livello 5", "Residuo"), 3);
     }
-    
-    public static boolean tryWriteFile(String filename, String data){
+
+    public static boolean tryWriteFile(String filename, String data) {
         try (PrintWriter out = new PrintWriter(filename)) {
             out.println(data);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
-        return true;    
+        return true;
     }
 }
